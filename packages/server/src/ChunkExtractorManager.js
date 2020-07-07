8acdfd928aae6f41bc0a06cebe389297ab3844c2
Alexander Naydenko <an@zeppelinen.com>
2020-07-07 12:23:01 +0200

    undefined (9d1b94f1f9b978074ae03d05ec54b8905c58cbbc)

--- packages/server/src/ChunkExtractorManager.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Context } from './sharedInternals'
-
-const ChunkExtractorManager = ({ extractor, children }) => (
-  <Context.Provider value={extractor}>{children}</Context.Provider>
-)
-
-export default ChunkExtractorManager
